19c13bae5dded021bf5953802af868ae06035ba9
Pozzitron1337 <the.vaho1337@gmail.com>
2022-05-01 16:48:01 +0300

    init2

---
+++ logging-service/src/main/java/LoggerController.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+public class LoggerController {
+    // private Map<UUID, String> messages = new ConcurrentHashMap();
+}
---
+++ logging-service/src/main/java/LoggerServiceApplication.java
@@ -0,0 +1,7 @@
+
+
+public class LoggerServiceApplication {
+    public static void main(String[] args) {
+
+    }
+}
